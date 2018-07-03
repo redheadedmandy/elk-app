21670ea06a9909d69f2ffa836c3ea874f3456da6
redheadedmandy <lily5lace@aol.com>
2018-07-03 10:04:48 -0400

    update

--- try-2/src/components/Loads.vue
+++ try-2/src/components/Loads.vue
@@ -6,11 +6,20 @@
 </template>
 
 <script>
-	import loadItem from '@/components/loadItem'
+import loadItem from '@/components/loadItem'
+import JQuery from 'jquery'
+let $ = JQuery
 export default {
   name: 'Loads',
   components: {loadItem}
 }
+$(document).ready(function() {
+	$.post("https://fleetr-208415.appspot.com/get-employee.php")
+	.done(function(data){
+		employees = JSON.parse(data);
+		document.write(employees);
+	});
+});
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
